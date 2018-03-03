0277f53c2e983d41ee5e2dfcf2ba445b3d528d46
Benjamin Raffle <benjamin.raffle@gmail.com>
2018-03-03 11:06:29 -0800

    Initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friends = [{ "name": "David", "photo": "https://pbs.twimg.com/profile_images/917109105965199360/KUd2j8D7_400x400.jpg", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] }, 
+{ "name": "Jack Palance", "photo": "https://i.ytimg.com/vi/VfXuLCd_9Ek/hqdefault.jpg", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] }, 
+{ "name": "Billiam J. Sonenclaire, III", "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2017-04/4/13/campaign_images/buzzfeed-prod-fastlane-02/49-british-swearwords-defined-2-12159-1491328043-2_dblbig.jpg", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] }, 
+{ "name": "Daniel Jackson", "photo": "https://www.planetclaire.tv/wp-content/uploads/1999/06/stargate-sg1-jackson-season-3.jpg", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, 
+{ "name": "Bill Preston, Esq.", "photo": "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-bill-and-teds-excellent-adventure-alex-winter.jpg", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] }, 
+{ "name": "Dick Clark's Mom", "photo": "https://upload.wikimedia.org/wikipedia/commons/5/55/Dame_Edna_%286959711768%29.jpg", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+
+module.exports = friends;
